5de949e9911c210f5fd11f5969f509a700052362
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-07-12 11:44:35 +0200

    Update ClusteringToMaTo.jl

--- src/ClusteringToMaTo.jl
+++ src/ClusteringToMaTo.jl
@@ -1,62 +1,56 @@
 module ClusteringToMaTo
 
 using DataStructures
-using Distances
 using NearestNeighbors
 
-export createGraph, createGraph2, densityf, data2clust
+export data2clust
 
-function createGraph(X::Array, k::Integer)
-    X = float.(X')
-    k = k + 1
-    kdtree = KDTree(X)
-    idxs, dists = knn(kdtree, X, k)
-    return idxs
-end
+function density_function( dists, dim, k)
 
-function createGraph2(X::Array, d::Number)
-    X = float.(X')
-    kdtree = KDTree(X)
-    idxs = inrange(kdtree, X, d)
-    return idxs
-end
-
-function densityf(X::Array, k::Int64 = 35)
-    k = k + 1
-    n = size(X, 1)
-    dim = size(X, 2)
-    X = float.(X')
-    kdtree = KDTree(X)
-    idxs, dists = knn(kdtree, X, k)
-    f = []
+    n = length(dists)
+    f = zeros(n)
+    
     if dim == 2
         for i = 1:n
-            push!(f, k * (k + 1) / (2 * n * pi * sum(dists[i] .^ 2)))
+            f[i] = k * (k + 1) / (2π * n * sum(dists[i] .^ 2))
         end
     elseif dim == 3
         for i = 1:n
-            push!(f, k * (k + 1) / (2 * n * (4 / 3) * pi * sum(dists[i] .^ 3)))
+            f[i] = k * (k + 1) / (2π * n * (4 / 3) * sum(dists[i] .^ 3))
         end
     elseif dim == 1
         for i = 1:n
-            push!(f, k * (k + 1) / (2 * n * sum(dists[i])))
+            f[i] = k * (k + 1) / (2 * n * sum(dists[i]))
         end
     end
+
     return f
+
 end
 
-function clustering(G::Array{Array{Int64,1},1}, f::Array, tao::Number)
-    n = length(f)
-    g = zeros(n)
-    v = [i for i = 1:n]
-    pair = [f v G]
+function data2clust(points::Array, graph = 1, k1 = 4, k2 = 4, τ = 0.01)
+
+    @assert graph in (1,2) "The variable `graph` should be 1 or 2"
+
+    dim, n  = size(points)
+    kdtree = KDTree(points)
+    k = k2 + 1
+    idxs, dists = knn(kdtree, points, k)
+
+    f = density_function(dists, dim, k)
+
+    if graph == 1
+        idxs, dists = knn(kdtree, points, k1+1)
+    elseif graph == 2
+        idxs = inrange(kdtree, points, k1)
+    end
+
+    g = zeros(Int, n)
+    v = collect(1:n)
+    pair = [f v idxs]
     pairs = sortslices(pair, dims = 1, rev = true, by = x -> x[1])
     vertices_corr_inv = Dict(zip(pairs[:, 2], 1:n))
-    ver_invf(x) = vertices_corr_inv[x]
-    C = []
-    for subset in pairs[:, 3]
-        push!(C, ver_invf.(subset))
-    end
+    C = [[vertices_corr_inv[i] for i in subset] for subset in pairs[:, 3]]
     pairs[:, 3] = C
     u = IntDisjointSets(n)
     for i = 1:n
@@ -70,7 +64,7 @@ function clustering(G::Array{Array{Int64,1},1}, f::Array, tao::Number)
             union!(u, ei, i)
             for j in nGi
                 e = find_root(u, j)
-                if e != ei && minimum([pairs[e, 1]; pairs[ei, 1]]) < pairs[i, 1] + tao
+                if e != ei && minimum([pairs[e, 1]; pairs[ei, 1]]) < pairs[i, 1] + τ
                     if pairs[e, 1] < pairs[ei, 1]
                         union!(u, ei, e)
                     else
@@ -82,28 +76,15 @@ function clustering(G::Array{Array{Int64,1},1}, f::Array, tao::Number)
             end
         end
     end
-    S = Set([find_root(u, i) for i = 1:n if pairs[find_root(u, i), 1] >= tao])
-    S2 = [s for s in S]
-    Xs = []
-    for j = 1:length(S2)
-        Xs = push!(
-            Xs,
-            (pairs[S2[j], 2], [pairs[i, 2] for i = 1:n if in_same_set(u, S2[j], i)]),
-        )
+    S = unique([find_root(u, i) for i = 1:n if pairs[find_root(u, i), 1] >= τ])
+    labels = zeros(Int, n)
+    for j = eachindex(S)
+        cluster = [pairs[i, 2] for i = 1:n if in_same_set(u, S[j], i)]
+        labels[cluster] .= j
     end
-    return Xs
-end
 
-function data2clust(data::Array, graph = 1, k1 = 4, k2 = 4, tao = 0.01)
-    f = densityf(data, k2)
-    if graph == 1
-        return clustering(createGraph(data, k1), f, tao)
-    elseif graph == 2
-        return clustering(createGraph2(data, k1), f, tao)
-    else
-        println("The 2nd varaible should be 1 or 2")
-        return 1
-    end
+    return labels
+
 end
 
 end
